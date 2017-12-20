a5b8460d5c902584630f618560d94498af537b61
GabrielBCompte <33900982+GabrielBCompte@users.noreply.github.com>
2017-12-21 00:09:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 if __name__ == '__main__':
     import docclass
     from twitterbot import TwitterBot
-    dc = docclass.NaiveBayes('test2.db')
-    larry = TwitterBot(docclass=dc, db_name='twitter3.db')
-    larry.update_trends_data(maximum=1)
+    from draw import Drawer
+    dc = docclass.NaiveBayes('dbname')
+    dc.make_tables() # No es necesario si se usa una base de datos ya entrenada
+    dc.load_data('csv_name') # No es necesario si se usa una base de datos ya entrenada
+    dw = Drawer()
+    larry = TwitterBot(docclass=dc, db_name='twitter13.db')
+    larry.make_tables()
+    larry.update_trends_data(maximum=10)
+    dw.draw_data(larry.get_currents_trends_data())
